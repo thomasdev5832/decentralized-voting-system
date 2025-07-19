14515ef74925916c4072459d00906538afbca3c2
thomasdev5832 <eugabrielthome@gmail.com>
2025-07-18 22:11:14 -0300

    first commit: install foundry; setup and clean frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='max-w-5xl min-h-screen mx-auto flex flex-col items-center justify-start p-4 text-center bg-neutral-950'>
+        <h1 className="text-3xl font-semibold text-green-400">
+          Decentralized Voting System
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
